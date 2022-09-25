f93dc83a65e2f7588a1aa8552066f27dc4674b66
Rony Garcia <ronygcgarcia@gmail.com>
2022-09-24 22:33:46 -0600

    feat: adds auth funcionality

---
+++ database/migrations/2022_09_21_021438_create_books_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('description');
+            $table->string('link_image');
+            $table->string('author');
+            $table->foreignId('genre_id')->constrained();
+            $table->integer('publish_year');
+            $table->integer('stock');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
