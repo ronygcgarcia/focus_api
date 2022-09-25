18488470ee9c7a34a9dff298b28da23c9633df0a
Rony Garcia <ronygcgarcia@gmail.com>
2022-09-25 01:08:16 -0600

    fix: changes import path of book

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Book;
+use App\Models\book as Book;
 use App\Models\Checkout;
 use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
