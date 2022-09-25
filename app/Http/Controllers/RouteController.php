efd0e8b5d8ce58f701f80be404e52f0b1ce67847
Rony Garcia <ronygcgarcia@gmail.com>
2022-09-24 23:35:00 -0600

    feat: adds routes index funcionality

--- app/Http/Controllers/RouteController.php
+++ app/Http/Controllers/RouteController.php
@@ -3,8 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Route;
 
 class RouteController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $user = Auth::user();
+        $roles =  $user->roles;
+        $routes = array();
+        foreach ($roles as $role) {
+            $route = Route::whereHas('roles', function ($query) use ($role) {
+                $query->where('roles.id', $role->id);
+            })
+                ->orderBy('orden')
+                ->get()->toArray();
+            $routes = array_merge($routes, $route);
+        }
+
+        return $routes;
+    }
 }
