316b537829e86c3bec942f253b4e5f760210bce5
Rony Garcia <ronygcgarcia@gmail.com>
2022-09-25 19:22:34 -0600

    feat: adds Login and User creation form request

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\UserStoreRequest;
 
 class UserController extends Controller
 {
@@ -17,22 +18,8 @@ class UserController extends Controller
         return UserResource::collection(User::all()->load('roles'));
     }
 
-    public function store(Request $request)
+    public function store(UserStoreRequest $request)
     {
-        $validator = Validator::make($request->all(), [
-            'email' => 'required|unique:users|max:64',
-            'first_name' => 'required|string',
-            'last_name' => 'required|string',
-            'password' => 'required|string|regex:/^.*(?=.{3,})(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[\d\x])(?=.*[!$#%]).*$/|confirmed',
-            'role_id' => 'required|integer'
-        ]);
-
-        if ($validator->fails()) {
-            return response()
-                ->json($validator->errors(), 422)
-                ->header('Content-Type', 'application/json');
-        }
-
         $request->merge(['password' => Hash::make($request->input('password'))]);
 
         $user = User::create($request->all());
---
+++ app/Http/Requests/UserStoreRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Validator;
+
+class UserStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'email' => 'required|unique:users|max:64',
+            'first_name' => 'required|string',
+            'last_name' => 'required|string',
+            'password' => 'required|string|regex:/^.*(?=.{3,})(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[\d\x])(?=.*[!$#%]).*$/|confirmed',
+            'role_id' => 'required|integer'
+        ];
+    }
+
+    public function withValidator(Validator $validator): void
+    {
+        $validator->after(function () {
+            $this->merge(['email' => strtolower($this->email)]);
+        });
+    }
+}
